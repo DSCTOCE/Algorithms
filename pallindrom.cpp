9e327275997335fad2556a3b99e88b3d206e9467
poornachandra kashi <44542685+poornachandrakashi@users.noreply.github.com>
2020-10-02 11:40:38 +0530

    Create pallindrom.cpp

---
+++ pallindrom.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+int main(){
+  int s= 1;
+  int e=n-1;
+  string = word;
+
+  printf("Please enter the string to test pallindrome");
+  strings(string,n,e);
+}
+
+
+bool strings(str string,int s,int e)
+{
+  if (s==e) return true;
+  if (string[s]!=string[e]) return false;
+  else{
+    return strings(string,s+1,e-1);
+  }
+}
