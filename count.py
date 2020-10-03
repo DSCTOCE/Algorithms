3fcdf187dd1cbf0e6548abc7c57397d000991831
poornachandra kashi <44542685+poornachandrakashi@users.noreply.github.com>
2020-10-03 20:31:02 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,12 @@
+# number=[]
+count=0
+for i in range(1,10):
+    if (i%2==0):
+        # number.append(i)
+        count=+1;
+        print(i)
+print(count)
+
+# print(number,end=" ")        
+# for numbers in number:
+#     print(number,end="")    
