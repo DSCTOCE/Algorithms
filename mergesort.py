5b601a6ecf9d8d1b88c6c4851bfe5f2e0a06bab8
sainag777 <46634798+sainag777@users.noreply.github.com>
2020-10-11 14:28:30 +0530

    Create mergesort.py
    Sorting refers to arranging data in a particular format. Sorting algorithm specifies the way to arrange data in a particular order. Most common orders are in numerical or lexicographical order.
    
    
    Merge sort first divides the array into equal halves and then combines them in a sorted manner.

---
+++ mergesort.py
@@ -0,0 +1,82 @@
+#merge sort pseudo code.
+"""procedure mergesort( var a as array )
+   if ( n == 1 ) return a
+
+   var l1 as array = a[0] ... a[n/2]
+   var l2 as array = a[n/2+1] ... a[n]
+
+   l1 = mergesort( l1 )
+   l2 = mergesort( l2 )
+
+   return merge( l1, l2 )
+end procedure
+
+procedure merge( var a as array, var b as array )
+
+   var c as array
+   while ( a and b have elements )
+      if ( a[0] > b[0] )
+         add b[0] to the end of c
+         remove b[0] from b
+      else
+         add a[0] to the end of c
+         remove a[0] from a
+      end if
+   end while
+   
+   while ( a has elements )
+      add a[0] to the end of c
+      remove a[0] from a
+   end while
+   
+   while ( b has elements )
+      add b[0] to the end of c
+      remove b[0] from b
+   end while
+   
+   return c
+	
+end procedure"""
+
+#algorithm
+
+def merge_sort(unsorted_list):
+    if len(unsorted_list) <= 1:
+        return unsorted_list
+# Find the middle point and devide it
+    middle = len(unsorted_list) // 2
+    left_list = unsorted_list[:middle]
+    right_list = unsorted_list[middle:]
+
+    left_list = merge_sort(left_list)
+    right_list = merge_sort(right_list)
+    return list(merge(left_list, right_list))
+
+# Merge the sorted halves
+
+def merge(left_half,right_half):
+
+    res = []
+    while len(left_half)!= 0 and len(right_half)!= 0:
+        if left_half[0] < right_half[0]:
+            res.append(left_half[0])
+            left_half.remove(left_half[0])
+        else:
+            res.append(right_half[0])
+            right_half.remove(right_half[0])
+    if len(left_half) == 0:
+        res = res + right_half
+    else:
+        res = res + left_half
+    return res
+
+
+unsorted list = [] 
+num= int(input("Enter number of elements"))
+
+print(f'Enter {num} elements')
+
+ for i in range(num):
+      data = int(input(f'{i+1}. ))
+      unsorted_list.append(data)
+print(f 'Unsorted list is{unsorted_list}') print(f'Sorted list is {merge_sort(unsorted_list)}')
