e0ede362adc5966378aa79b937326c606e6be38e
n s dhanush <50840045+nsdhanush@users.noreply.github.com>
2020-10-11 15:07:19 +0530

    Create bucketsort.py
    Bucket Sort is a sorting technique that sorts the elements by first dividing the elements into several groups called buckets. The elements inside each bucket are sorted using any of the suitable sorting algorithms or recursively calling the same algorithm.

---
+++ bucketsort.py
@@ -0,0 +1,54 @@
+#Bucket Sort Algorithm
+
+"""Bucket Sort is a sorting technique that sorts the elements by first dividing the elements into several groups called buckets. 
+The elements inside each bucket are sorted using any of the suitable sorting algorithms or recursively calling the same algorithm.
+"""
+
+#Pseudo code
+
+"""
+bucketSort()
+  create N buckets each of which can hold a range of values
+  for all the buckets
+    initialize each bucket with 0 values
+  for all the buckets
+    put elements into buckets matching the range
+  for all the buckets 
+    sort elements in each bucket
+  gather elements from each bucket
+end bucketSort
+
+"""
+
+
+# Bucket Sort in Python
+
+
+def bucketSort(array):
+    bucket = []
+
+    # Create empty buckets
+    for i in range(len(array)):
+        bucket.append([])
+
+    # Insert elements into their respective buckets
+    for j in array:
+        index_b = int(10 * j)
+        bucket[index_b].append(j)
+
+    # Sort the elements of each bucket
+    for i in range(len(array)):
+        bucket[i] = sorted(bucket[i])
+
+    # Get the sorted elements
+    k = 0
+    for i in range(len(array)):
+        for j in range(len(bucket[i])):
+            array[k] = bucket[i][j]
+            k += 1
+    return array
+
+
+array = [.42, .32, .33, .52, .37, .47, .51]
+print("Sorted Array in descending order is")
+print(bucketSort(array))
